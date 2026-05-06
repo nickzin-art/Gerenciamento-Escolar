d6935f7c9b8bb8827ba3df9ea9e0ce94f9b9e321
nickzin-art <nicolasbez2009.11@gmail.com>
2026-05-06 11:23:13 -0300

    commit feito

--- App/model/reportModel.py
+++ App/model/reportModel.py
@@ -117,6 +117,11 @@ class Report:
         return cls._getObjectList(result)
  
 if __name__ == "__main__":
-    relatorio_para_editar = Report(id=7, description="Descrição atualizada com sucesso")
-    Report.edit(relatorio_para_editar)
-    print(relatorio_para_editar)
+
+    c = Report.searchParentID(1)
+    print(c)
+
+
+    #relatorio_para_editar = Report(id=7, description="Descrição atualizada com sucesso")
+    #Report.edit(relatorio_para_editar)
+    #print(relatorio_para_editar)
